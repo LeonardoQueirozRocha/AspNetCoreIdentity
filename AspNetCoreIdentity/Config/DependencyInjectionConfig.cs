cbb589591cb87c711d62dd7e3529ba9204f20de0
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2021-09-24 19:22:17 -0300

    Estudos sobre o Identity

---
+++ AspNetCoreIdentity/Config/DependencyInjectionConfig.cs
@@ -0,0 +1,26 @@
+﻿using AspNetCoreIdentity.Extensions;
+using KissLog;
+using KissLog.AspNetCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreIdentity.Config
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddSingleton<IAuthorizationHandler, PermissaoNecessariaHandler>();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped((context) => Logger.Factory.Get());
+
+            services.AddLogging(logging => logging.AddKissLog());
+
+            services.AddScoped<AuditoriaFilter>();
+
+            return services;
+        }
+    }
+}
